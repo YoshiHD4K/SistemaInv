59a9bb09e4a8909283cdf755c8842e175d8133bd
YoshiHD4K <tomyreveroncito@gmail.com>
2025-06-02 22:16:49 -0400

    Actualiza el archivo SQL: corrige la fecha de generación y elimina tablas y datos obsoletos.

--- basededatos/sisteminv.sql
+++ basededatos/sisteminv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 26-05-2025 a las 12:02:32
+-- Tiempo de generación: 03-06-2025 a las 02:15:55
 -- Versión del servidor: 9.1.0
 -- Versión de PHP: 8.3.14
 
@@ -24,99 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clientes`
---
-
-DROP TABLE IF EXISTS `clientes`;
-CREATE TABLE IF NOT EXISTS `clientes` (
-  `ID` int NOT NULL,
-  `Nombre` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `Apellido` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `Tipo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `Cedula` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `Telefono` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `Direccion` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`ID`, `Nombre`, `Apellido`, `Tipo`, `Cedula`, `Telefono`, `Direccion`) VALUES
-(1, 'Tomas', 'Reveron', 'V', '29989547', '04122884386', 'Caricuao'),
-(2, 'Merlin', 'Lopez', 'V', '14446246', '04127395093', 'Caricuao'),
-(0, 'Daniela', 'Garcia', 'V', '30224413', '04241569878', 'Guaicoco');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `productos`
 --
 
 DROP TABLE IF EXISTS `productos`;
 CREATE TABLE IF NOT EXISTS `productos` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Producto` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `CantidadDisp` int NOT NULL,
-  `Precio` int NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Descripcion` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Precio` float NOT NULL,
+  `Cantidad` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`ID`, `Producto`, `CantidadDisp`, `Precio`) VALUES
-(1, 'Cambur', 100, 4);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `Nombre_Usuario` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Contraseña` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Correo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Tipo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`Id`, `Nombre_Usuario`, `Contraseña`, `Correo`, `Tipo`) VALUES
-(1, 'TReveron', '**TASM2077**', 'tomyreveroncito@gmail.com', 'cajero');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proveedores`
---
-
-DROP TABLE IF EXISTS `proveedores`;
-CREATE TABLE IF NOT EXISTS `proveedores` (
-  
-  `nombre del proveedor` VARCHAR(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `direccion del proveedor` VARCHAR(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `nro de telefono del proveedor` INT(11) COLLATE utf8mb4_general_ci NOT NULL,
-  `rif_proveedor` VARCHAR(10) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`nomre del proveedor`),
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `proveedores`
---
-
-INSERT INTO `proveedores` (`nombre del proveedor`, `direccion del proveedor`, `nro de telefono del proveedor`, `rif del proveedor`) VALUES
-(1, 'Proveedor1', 'Direccion1', '02121234567', 'J-12345678-9'),
-(2, 'Proveedor2', 'Direccion2', '02121234568', 'J-98765432-1');
-
+INSERT INTO `productos` (`Nombre`, `Descripcion`, `Precio`, `Cantidad`) VALUES
+('Mangos', 'Mango', 0.13, 45),
+('Cambur', 'cambur', 1.63, 8);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
